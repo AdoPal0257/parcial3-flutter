49fb1f0e445d2aa306eb81b3a684933fd67998d4
Adonay <adonaypalacios98@gmail.com>
2021-10-20 23:47:38 -0600

    second commit

---
+++ android/app/src/main/kotlin/com/example/parcial3_kelvin_palacios/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parcial3_kelvin_palacios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/database.dart
@@ -0,0 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _mainCollection = _firestore.collection('notes');
+
+class Database {
+  static String? userUid;
+
+// ...
+}
